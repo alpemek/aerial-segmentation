5c1d7ac43249a59f42a094880aa51e0a1fb5e9e9
hos-b <mh.bhdrn@gmail.com>
2020-07-19 19:18:51 +0200

    Add download_dataset.sh

---
+++ dataset/download_dataset.sh
@@ -0,0 +1,8 @@
+wget https://zenodo.org/record/1154821/files/berlin.zip
+unzip berlin.zip && rm berlin.zip
+wget https://zenodo.org/record/1154821/files/chicago.zip
+unzip chicago.zip &&  rm chicago.zip
+wget https://zenodo.org/record/1154821/files/paris.zip
+unzip paris.zip && rm paris.zip
+wget https://zenodo.org/record/1154821/files/zurich.zip
+unzip zurich.zip && rm zurich.zip
